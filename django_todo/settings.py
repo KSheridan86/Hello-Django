458e8ffc0d44a64c47e4b58d670a0c83e8db6118
Ken Sheridan <kensheridan86@gmail.com>
2022-08-07 18:04:14 +0000

    fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-gb)o_yrak)6#wt@4sr8_snmp(#ii9ggu(1k+9-&bcje=y3$)^9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ci-django-todo-69.herokuapp.com'
+]
 
 
 # Application definition
